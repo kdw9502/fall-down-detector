b0e303ba6b1762288c886c5a74d3f7b8876985f4
kdw9502 <kdw9502@gmail.com>
2020-05-13 13:13:40 +0900

    add sms alert script for demo

--- server/database_access.py
+++ server/database_access.py
@@ -65,15 +65,6 @@ def respond(err, res=None):
 
 
 def lambda_handler(event, context):
-    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
-    access to the request and response payload, including headers and
-    status code.
-
-    To scan a DynamoDB table, make a GET request with the TableName as a
-    query string parameter. To put, update, or delete an item, make a POST,
-    PUT, or DELETE request respectively, passing in the payload to the
-    DynamoDB API as a JSON body.
-    '''
     # print("Received event: " + json.dumps(event, indent=2))
 
     operation = event['httpMethod']

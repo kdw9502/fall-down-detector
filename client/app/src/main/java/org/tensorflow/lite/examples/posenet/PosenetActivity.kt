46f304489f657f1f6bcde1b0ac35adee3813cdad
kdw9502 <kdw9502@gmail.com>
2020-06-21 21:56:17 +0900

    change camera direction

--- client/app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
+++ client/app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
@@ -289,7 +289,7 @@ class PosenetActivity :
 
         // We don't use a front facing camera in this sample.
         val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
-        if (cameraDirection != null && cameraDirection == CameraCharacteristics.LENS_FACING_BACK)//후면은 LENS_FACING_FRONT
+        if (cameraDirection != null && cameraDirection == CameraCharacteristics.LENS_FACING_FRONT)//후면은 LENS_FACING_FRONT
         {
           continue
         }
@@ -443,10 +443,10 @@ class PosenetActivity :
       // Create rotated version for portrait display, 뷰 이미지 조정하는곳.
       val rotateMatrix = Matrix()
       //후면
-      //rotateMatrix.postRotate(90.0f)
+      rotateMatrix.postRotate(90.0f)
       //정면
-      rotateMatrix.postRotate(270.0f)
-      rotateMatrix.postScale(-1.0f, 1.0f)
+      //      rotateMatrix.postRotate(270.0f)
+      //      rotateMatrix.postScale(-1.0f, 1.0f)
       // rotateMatrix.
 
 
